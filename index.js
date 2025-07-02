7ea76febbb1a8d036641f30b855999a3bdedcce0
FullofMyself <wmaseko1@live.com>
2025-07-02 23:26:30 +0200

    Update payment portal backend with Yoco integration and CORS fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://fullomyself.github.io"
+    "https://FulloMyself.github.io"
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"]
